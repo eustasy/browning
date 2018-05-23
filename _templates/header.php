e01334d42569846f3616da6600e080d337b57046
Lewis Goddard <lewisgoddard@eustasy.org>
2018-05-23 09:46:37 +0100

    Update header.php

--- _templates/header.php
+++ _templates/header.php
@@ -10,7 +10,7 @@
 		<meta name="keywords" content="Colors.css material flat ui colors plugin css eustasy Labs">
 		<meta name="description" content="Colors.css is a small CSS file for Flat UI Colors in any application without loading a larger material design stylesheet.">
 		<title>Browning - A Lightweight Mailgun Script &nbsp;&middot;&nbsp; eustasy Labs</title>
-		<link rel="stylesheet" href="https://cdn.jsdelivr.net/g/normalize,colors.css">
+		<link rel="stylesheet" href="https://cdn.jsdelivr.net/combine/gh/eustasy/Colors.css@1/colors.min.css,gh/necolas/normalize.css@8/normalize.min.css">
 		<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:300,400|Lusitana|Source+Code+Pro" data-noprefix>
 		<link rel="stylesheet" href="https://labs.eustasy.org/assets/css/grid.min.css">
 		<link rel="stylesheet" href="https://labs.eustasy.org/assets/css/labs.css">
@@ -23,7 +23,7 @@
 			ga('send', 'pageview');
 		</script>
 		<script>var jQl={q:[],dq:[],gs:[],ready:function(a){'function'==typeof a&&jQl.q.push(a);return jQl},getScript:function(a,c){jQl.gs.push([a,c])},unq:function(){for(var a=0;a<jQl.q.length;a++)jQl.q[a]();jQl.q=[]},ungs:function(){for(var a=0;a<jQl.gs.length;a++)jQuery.getScript(jQl.gs[a][0],jQl.gs[a][1]);jQl.gs=[]},bId:null,boot:function(a){'undefined'==typeof window.jQuery.fn?jQl.bId||(jQl.bId=setInterval(function(){jQl.boot(a)},25)):(jQl.bId&&clearInterval(jQl.bId),jQl.bId=0,jQl.unqjQdep(),jQl.ungs(),jQuery(jQl.unq()), 'function'==typeof a&&a())},booted:function(){return 0===jQl.bId},loadjQ:function(a,c){setTimeout(function(){var b=document.createElement('script');b.src=a;document.getElementsByTagName('head')[0].appendChild(b)},1);jQl.boot(c)},loadjQdep:function(a){jQl.loadxhr(a,jQl.qdep)},qdep:function(a){a&&('undefined'!==typeof window.jQuery.fn&&!jQl.dq.length?jQl.rs(a):jQl.dq.push(a))},unqjQdep:function(){if('undefined'==typeof window.jQuery.fn)setTimeout(jQl.unqjQdep,50);else{for(var a=0;a<jQl.dq.length;a++)jQl.rs(jQl.dq[a]); jQl.dq=[]}},rs:function(a){var c=document.createElement('script');document.getElementsByTagName('head')[0].appendChild(c);c.text=a},loadxhr:function(a,c){var b;b=jQl.getxo();b.onreadystatechange=function(){4!=b.readyState||200!=b.status||c(b.responseText,a)};try{b.open('GET',a,!0),b.send('')}catch(d){}},getxo:function(){var a=!1;try{a=new XMLHttpRequest}catch(c){for(var b=['MSXML2.XMLHTTP.5.0','MSXML2.XMLHTTP.4.0','MSXML2.XMLHTTP.3.0','MSXML2.XMLHTTP','Microsoft.XMLHTTP'],d=0;d<b.length;++d){try{a= new ActiveXObject(b[d])}catch(e){continue}break}}finally{return a}}};if('undefined'==typeof window.jQuery){var $=jQl.ready,jQuery=$;$.getScript=jQl.getScript};</script>
-		<script>jQl.loadjQ('https://cdn.jsdelivr.net/g/prefixfree,jquery,jquery.equalize,jquery.downboy,jquery.autosize');</script>
+		<script>jQl.loadjQ('https://cdn.jsdelivr.net/combine/gh/jquery/jquery@3/dist/jquery.min.js,gh/LeaVerou/prefixfree@1/prefixfree.min.js,gh/jackmoore/autosize@4/dist/autosize.min.js,gh/eustasy/jQuery.equalize@1/jquery.equalize.min.js,gh/eustasy/jQuery.downBoy@1/jquery.downboy.min.js');</script>
 		<script>$(function(){autosize($('textarea'));});</script>
 		<script>$(function(){
 				$('textarea').one('click',function(){$(this).select();});
